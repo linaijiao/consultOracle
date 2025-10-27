36d02bbe2a9c327af4e532368c7e9a2d1e18b63c
cirulus <66499392+cirulus@users.noreply.github.com>
2025-10-27 21:38:26 +0800

    test1111

--- MyApp/app/src/main/java/com/bhcj/telling/model/bean/GuaWords.java
+++ MyApp/app/src/main/java/com/bhcj/telling/model/bean/GuaWords.java
@@ -15,7 +15,7 @@ public class GuaWords implements Serializable {
 
     /** 卦象名称 */
     private String name;
-    11122223333333334555
+    11122223333333334555333
     /** 卦象标题/序号 */
     private String title;
     
