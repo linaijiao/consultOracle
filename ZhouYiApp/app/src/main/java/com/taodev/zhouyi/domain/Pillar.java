a64a93b267e72794401809fda3fa1b84c85687d6
cirulus <66499392+cirulus@users.noreply.github.com>
2025-12-30 17:57:11 +0800

    添加成员变量天干与地支 添加getset方法 构造方法初始化成员变量

--- ZhouYiApp/app/src/main/java/com/taodev/zhouyi/domain/Pillar.java
+++ ZhouYiApp/app/src/main/java/com/taodev/zhouyi/domain/Pillar.java
@@ -1,6 +1,29 @@
 package com.taodev.zhouyi.domain;
 
 public class Pillar {
+    //天干
+    private String stem;
+    //地支
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    private String branch;
+
+    public String getStem() {
+        return stem;
+    }
+
+    public void setStem(String stem) {
+        this.stem = stem;
+    }
+
     public Pillar(String stem, String branch) {
+        this.stem = stem;
+        this.branch = branch;
     }
 }
