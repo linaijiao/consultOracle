76738a23b02692027fd58e2874db0c58ade8de6b
linaijiao <lin.aijiao@163.com>
2025-12-31 17:18:45 +0800

    完善城市下拉框功能

--- ZhouYiApp/app/src/main/java/com/taodev/zhouyi/fourpillars/ui/FourPillarsActivity.java
+++ ZhouYiApp/app/src/main/java/com/taodev/zhouyi/fourpillars/ui/FourPillarsActivity.java
@@ -5,12 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -33,6 +35,16 @@ public class FourPillarsActivity extends AppCompatActivity {
     private Spinner spinnerMinute;
     private Button buttonSubmit;
 
+    // 省份数组
+    private String[] provinces;
+
+    // 城市二维数组，与省份一一对应
+    private String[][] cities;
+
+    // 当前选中的省份和城市
+    private String selectedProvince = "";
+    private String selectedCity = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +62,8 @@ public class FourPillarsActivity extends AppCompatActivity {
         spinnerHour = findViewById(R.id.spinnerHour);
         spinnerMinute = findViewById(R.id.spinnerMinute);
         buttonSubmit = findViewById(R.id.buttonSubmit);
+
+
 
         // Initialize Year Spinner
         List<String> years = new ArrayList<>();
@@ -97,25 +111,19 @@ public class FourPillarsActivity extends AppCompatActivity {
         minuteAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMinute.setAdapter(minuteAdapter);
 
-        // Province Spinner Listener (You would need to fetch the corresponding cities)
-        spinnerProvince.setOnItemSelectedListener(new android.widget.AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(android.widget.AdapterView<?> parent, View view, int position, long id) {
-                String selectedProvince = parent.getItemAtPosition(position).toString();
-                //TODO: Fetch cities based on selected province and populate the city spinner
-                List<String> cityList = new ArrayList<>();
-                cityList.add("北京"); //Sample city to show the spinner working
-                ArrayAdapter<String> cityAdapter = new ArrayAdapter<>(FourPillarsActivity.this, android.R.layout.simple_spinner_item, cityList);
-                cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinnerCity.setAdapter(cityAdapter);
-            }
+//        // 设置省份数据
+//        ArrayAdapter<CharSequence> provinceAdapter = ArrayAdapter.createFromResource(this,
+//                R.array.province_array, android.R.layout.simple_spinner_item);
+//        provinceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinnerProvince.setAdapter(provinceAdapter);
+        //初始化城市数据
+        initCiryData();
+        //
+        setupSpinners();
+        setupListeners();
 
-            @Override
-            public void onNothingSelected(android.widget.AdapterView<?> parent) {
 
-            }
-        });
-
+        //开始排八字
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -156,4 +164,134 @@ public class FourPillarsActivity extends AppCompatActivity {
             }
         });
     }
+
+    //初始化城市数据
+    private void initCiryData() {
+        // 从arrays.xml中获取省份数据
+        provinces = getResources().getStringArray(R.array.province_array);
+
+        // 初始化城市二维数组
+        cities = new String[provinces.length][];
+
+        // 为每个省份加载对应的城市数组
+        for (int i = 0; i < provinces.length; i++) {
+            String provinceName = provinces[i];
+            String arrayName = getCityArrayName(provinceName);
+            int arrayId = getResources().getIdentifier(arrayName, "array", getPackageName());
+
+            if (arrayId != 0) {
+                cities[i] = getResources().getStringArray(arrayId);
+            } else {
+                // 如果没有找到对应的城市数组，使用空数组
+                cities[i] = new String[]{"请选择城市"};
+            }
+        }
+    }
+
+    /**
+     * 根据省份名称获取对应的城市数组名称
+     */
+    private String getCityArrayName(String provinceName) {
+        switch (provinceName) {
+            case "北京市":
+                return "cities_beijing";
+            case "上海市":
+                return "cities_shanghai";
+            case "广东省":
+                return "cities_guangdong";
+            case "江苏省":
+                return "cities_jiangsu";
+            case "浙江省":
+                return "cities_zhejiang";
+            case "四川省":
+                return "cities_sichuan";
+            case "湖北省":
+                return "cities_hubei";
+            case "山东省":
+                return "cities_shandong";
+            default:
+                return "";
+        }
+    }
+    private void setupSpinners() {
+        // 设置省份Spinner的适配器
+        ArrayAdapter<String> provinceAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                provinces
+        );
+        provinceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerProvince.setAdapter(provinceAdapter);
+
+        // 设置城市Spinner的适配器（初始为空）
+        ArrayAdapter<String> cityAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                new String[]{"请选择城市"}
+        );
+        cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCity.setAdapter(cityAdapter);
+
+        // 默认选择第一个（"请选择省份"）
+        spinnerProvince.setSelection(0);
+        spinnerCity.setSelection(0);
+    }
+
+    private void setupListeners() {
+        // 省份选择监听
+        spinnerProvince.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    // 选择了"请选择省份"
+                    selectedProvince = "";
+                    updateCitySpinner(new String[]{"请选择城市"});
+                } else {
+                    selectedProvince = provinces[position];
+                    updateCitySpinner(cities[position]);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // 什么都不做
+            }
+        });
+
+        // 城市选择监听
+        spinnerCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    // 选择了"请选择城市"
+                    selectedCity = "";
+                } else {
+                    String[] cityArray = cities[spinnerProvince.getSelectedItemPosition()];
+                    selectedCity = cityArray[position];
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // 什么都不做
+            }
+        });
+
+    }
+    /**
+     * 更新城市Spinner的数据
+     */
+    private void updateCitySpinner(String[] cityArray) {
+        ArrayAdapter<String> cityAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                cityArray
+        );
+        cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCity.setAdapter(cityAdapter);
+        spinnerCity.setSelection(0);
+    }
+
+
+
 }
