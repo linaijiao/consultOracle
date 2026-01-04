7ea7aac0f153914c61fc2e134abc2c80bf751a67
cirulus <jok45@163.com>
2026-01-04 23:26:13 +0800

    接口增加方法

--- ZhouYiApp/app/src/main/java/com/taodev/zhouyi/engine/IFourPillarsAnalysisService.java
+++ ZhouYiApp/app/src/main/java/com/taodev/zhouyi/engine/IFourPillarsAnalysisService.java
@@ -1,11 +1,14 @@
 package com.taodev.zhouyi.engine;
 
+import com.taodev.zhouyi.domain.FourPillarsResult;
 import com.taodev.zhouyi.domain.Pillar;
 import com.taodev.zhouyi.domain.LuckPillar;
 import java.util.List;
 import java.util.Map;
 
 public interface IFourPillarsAnalysisService {
+    /** 总入口 */
+     void analyze(FourPillarsResult result);
 
     /** 计算大运（10年一运） */
     List<LuckPillar> calculateLuckPillars(Pillar yearPillar, Pillar monthPillar, boolean isMale, int startAge);
@@ -15,4 +18,6 @@ public interface IFourPillarsAnalysisService {
 
     /** 计算十神统计 */
     Map<String, Integer> calculateTenGods(Pillar dayPillar, Pillar[] allPillars);
+    /** 计算纳音和空亡 */
+    void calculateNaYinAndKongWang(FourPillarsResult result);
 }
