450c824796c5be0d2ce3d01f14a98b7e71a8c6fc
cirulus <jok45@163.com>
2026-01-04 23:19:07 +0800

    新增输入UImodel

---
+++ ZhouYiApp/app/src/main/java/com/taodev/zhouyi/fourpillars/ui/FourPillarsInputUiModel.java
@@ -0,0 +1,35 @@
+package com.taodev.zhouyi.fourpillars.ui;
+
+import com.taodev.zhouyi.domain.Gender;
+
+public class FourPillarsInputUiModel {
+    // 基础信息
+    public String name;
+    public Gender gender;
+
+    // 时间信息
+    public int year;
+    public int month;
+    public int day;
+    public int hour;
+    public int minute;
+
+    // 地点信息 (用于后续算真太阳时)
+    public String province;
+    public String city;
+
+    // 构造函数：对应您 Activity 里的传参顺序
+    public FourPillarsInputUiModel(String name, Gender gender,
+                                   int year, int month, int day, int hour, int minute,
+                                   String province, String city) {
+        this.name = name;
+        this.gender=gender;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+        this.province = province;
+        this.city = city;
+    }
+}
