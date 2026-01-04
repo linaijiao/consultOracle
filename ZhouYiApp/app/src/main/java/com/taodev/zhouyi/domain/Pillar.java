a6019cd79513e6cc33019e4f133fac01fc9a8ab2
cirulus <jok45@163.com>
2026-01-04 09:05:56 +0800

    增加十神藏干空亡长生

--- ZhouYiApp/app/src/main/java/com/taodev/zhouyi/domain/Pillar.java
+++ ZhouYiApp/app/src/main/java/com/taodev/zhouyi/domain/Pillar.java
@@ -1,9 +1,79 @@
 package com.taodev.zhouyi.domain;
 
+import java.util.Collection;
+import java.util.List;
+
 public class Pillar {
     //天干
     private String stem;
+    private String branch;
+
+    public List<String> getHiddenStems() {
+        return hiddenStems;
+    }
+
+    // 四柱天干的十神
+    private String stemTenGod;
+    // 藏干
+    private List<String> hiddenStems;
+
+    // 四柱地支隐藏的天干的十神；也就是藏干的十神
+    // 地支藏干的十神列表 (可能有 1~3 个)
+    // 对象，包含：天干名 + 十神名
+    public List<HiddenStemInfo> hiddenStemInfos;
+
+    // 存放 "长生", "帝旺" 等
+    public String lifeStage;
+
+    public String getLifeStage() {
+        return lifeStage;
+    }
+
+    // 纳音 (例如 "海中金")
+    private String naYin;
+
+    //空亡
+    public void setLifeStage(String lifeStage) {
+        this.lifeStage = lifeStage;
+    }
+
+    public List<HiddenStemInfo> getHiddenStemInfos() {
+        return hiddenStemInfos;
+    }
+
+    public void setHiddenStemInfos(List<HiddenStemInfo> hiddenStemInfos) {
+        this.hiddenStemInfos = hiddenStemInfos;
+    }
+
+    public String getNaYin() {
+        return naYin;
+    }
+
+    public void setNaYin(String naYin) {
+        this.naYin = naYin;
+    }
+
+    public String getKongWang() {
+        return kongWang;
+    }
+
+    public void setKongWang(String kongWang) {
+        this.kongWang = kongWang;
+    }
+
+    // 空亡 (例如 "戌亥")
+    private String kongWang;
+
+    public String getStemTenGod() {
+        return stemTenGod;
+    }
+
+    public void setStemTenGod(String stemTenGod) {
+        this.stemTenGod = stemTenGod;
+    }
+    //天干十神
     //地支
+
     public String getBranch() {
         return branch;
     }
@@ -11,8 +81,6 @@ public class Pillar {
     public void setBranch(String branch) {
         this.branch = branch;
     }
-
-    private String branch;
 
     public String getStem() {
         return stem;
@@ -26,4 +94,19 @@ public class Pillar {
         this.stem = stem;
         this.branch = branch;
     }
+    public List<String> HiddenStemInfo(){
+        return hiddenStems;
+    }
+
+    public void setHiddenStems(List<HiddenStemInfo> infos) {
+        this.hiddenStemInfos = infos;
+    }
+
+    public static class HiddenStemInfo {
+        // 藏干天干 (如 "丙")
+        public String stemName;
+        // 对应的十神 (如 "食神")
+        public String tenGodName;
+    }
+
 }
