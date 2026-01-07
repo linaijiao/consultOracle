57ba2d31263b07ebae25e17e47e35af8f92fb369
cirulus <jok45@163.com>
2026-01-07 09:59:52 +0800

    1、调用方法baziFacade改为调fourPillarsService baziFacade没有保存数据功能
    2、convert改为convertPillar

--- ZhouYiApp/app/src/main/java/com/taodev/zhouyi/fourpillars/ui/FourPillarsViewModel.java
+++ ZhouYiApp/app/src/main/java/com/taodev/zhouyi/fourpillars/ui/FourPillarsViewModel.java
@@ -1,12 +1,17 @@
 package com.taodev.zhouyi.fourpillars.ui;
 
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel; // 注意：用 AndroidViewModel 才能拿 Context
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.taodev.zhouyi.calendar.BaziAttributesCalculator;
+import com.taodev.zhouyi.calendar.LuckPillarCalculator;
+import com.taodev.zhouyi.calendar.TenGodCalculator;
 import com.taodev.zhouyi.core.repository.BaziDataLoader;
 import com.taodev.zhouyi.domain.BaziRules;
 import com.taodev.zhouyi.domain.FourPillarsInput;
@@ -14,6 +19,8 @@ import com.taodev.zhouyi.domain.FourPillarsResult;
 import com.taodev.zhouyi.facade.BaziFacade;
 import com.taodev.zhouyi.calendar.CommonCalendarService;
 import com.taodev.zhouyi.fourpillars.analysis.FourPillarsAnalysisService;
+import com.taodev.zhouyi.fourpillars.data.FourPillarsRepository;
+import com.taodev.zhouyi.fourpillars.service.FourPillarsService;
 import com.taodev.zhouyi.fourpillars.ui.DisplayConverter;
 import com.taodev.zhouyi.fourpillars.ui.FourPillarsDisplayModel;
 import com.taodev.zhouyi.fourpillars.ui.FourPillarsInputUiModel;
@@ -22,11 +29,11 @@ public class FourPillarsViewModel extends AndroidViewModel {
 
     // 给 Activity 观察的数据
     public final MutableLiveData<FourPillarsDisplayModel> displayData = new MutableLiveData<>();
-
-    // 我们的总指挥
+    private FourPillarsService fourPillarsService;
+    //
     private BaziFacade baziFacade;
 
-    public FourPillarsViewModel(Application application) {
+    public FourPillarsViewModel(@NonNull Application application) {
         super(application);
         initBaziEngine();
     }
@@ -39,26 +46,49 @@ public class FourPillarsViewModel extends AndroidViewModel {
         // B. 组装服务
         CommonCalendarService calendarService = new CommonCalendarService(getApplication());
         FourPillarsAnalysisService analysisService = new FourPillarsAnalysisService(rules);
+        BaziAttributesCalculator baziAttributesCalculator = new BaziAttributesCalculator(rules);
+        TenGodCalculator tenGodCalculator = new TenGodCalculator(rules);
+        LuckPillarCalculator luckPillarCalculator = new LuckPillarCalculator(rules,baziAttributesCalculator,tenGodCalculator);
+        FourPillarsRepository repository = new FourPillarsRepository(getApplication());
 
         // C. 创建 Facade
-        baziFacade = new BaziFacade(calendarService, analysisService);
+//        baziFacade = new BaziFacade(calendarService, analysisService);
+         fourPillarsService = new FourPillarsService(
+                calendarService,
+                analysisService,
+                luckPillarCalculator,
+                repository,
+                rules);
+
     }
 
     // 2. 响应用户点击“排盘”
     public void calculate(FourPillarsInputUiModel uiInput) {
         new Thread(() -> {
             try {
+                Log.d("VIEWMODEL_CALCULATE", "uiInput！ :" +
+                        uiInput.year+ uiInput.month+ uiInput.day+
+                        uiInput.hour+uiInput.minute+ uiInput.gender);
                 // A. 把 UI 模型转成 领域模型 (Input)
                 FourPillarsInput domainInput = new FourPillarsInput(
                         uiInput.year, uiInput.month, uiInput.day, uiInput.hour,uiInput.minute, uiInput.gender
                 );
 
                 // B. 【核心】调用 Facade 进行计算
-                FourPillarsResult result = baziFacade.performFullAnalysis(domainInput);
+//                FourPillarsResult result = baziFacade.performFullAnalysis(domainInput);
+                FourPillarsResult result = fourPillarsService.calculateFourPillars(domainInput);
+                Log.d("VIEWMODEL_CALCULATE", "reslut！ :" +result.getDayPillar().getStem());
+                Log.d("VIEWMODEL_CALCULATE", "reslut！ :" +result.getDayPillar().getBranch());
+                Log.d("VIEWMODEL_CALCULATE", "reslut！ :" +result.getDayPillar().getStem());
+                Log.d("VIEWMODEL_CALCULATE", "reslut！ :" +result.getDayPillar().getKongWang());
+                Log.d("VIEWMODEL_CALCULATE", "reslut！ :" +result.getDayPillar().getNaYin());
 
+                Log.d("VIEWMODEL_CALCULATE", "reslut！ :" +result.getDayPillar().getLifeStage());
+                Log.d("VIEWMODEL_CALCULATE", "reslut！ :" +result.getDayPillar().getStemTenGod());
+                Log.d("VIEWMODEL_CALCULATE", "reslut！ :" +result.getDayPillar().getHiddenStemInfos());
                 // C. 【转换】把计算结果 转成 界面显示模型
-                FourPillarsDisplayModel displayModel = DisplayConverter.convert(result);
-                Log.d("FourPillarsViewModel", "reslut！ :" +displayModel.fourPillars.get(0));
+                FourPillarsDisplayModel displayModel = DisplayConverter.convertPillar(result);
+                Log.d("VIEWMODEL_CALCULATE", "reslut！ :" +displayModel.pillars.get(0));
 
                 // D. 更新 LiveData，通知 Activity 刷新
 //                displayData.setValue(displayModel);
