636239a8afa6976a8750bca226ec931d9863fcee
cirulus <jok45@163.com>
2026-01-10 19:00:53 +0800

    时区转换，除了默认增加城市选择

--- ZhouYiApp/app/src/main/java/com/taodev/zhouyi/calendar/TimeConverter.java
+++ ZhouYiApp/app/src/main/java/com/taodev/zhouyi/calendar/TimeConverter.java
@@ -49,7 +49,7 @@ public class TimeConverter {
             return null; // 或者抛异常
         }
         //获取时区/城市
-        String timezoneIdStr = input.getTimezoneIdStr();
+        String timezoneIdStr = input.getTimezoneIdStr(input.getCity());
         //时区对象
         ZoneId zoneId = ZoneId.of(timezoneIdStr);
         //1、本地时间 → UTC 时间（考虑时区）
